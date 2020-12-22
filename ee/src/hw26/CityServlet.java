0d38dfb5b38119f314a057c3d8edd24f6d4a02ca
Michael <cresedel211194@gmail.com>
2020-12-22 19:19:49 +0200

    made hw 26 delete sql added

--- ee/src/hw26/CityServlet.java
+++ ee/src/hw26/CityServlet.java
@@ -10,27 +10,33 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 @WebServlet("/city")
 public class CityServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Integer> list = new ArrayList<>();
+
         StringBuilder stringBuilder = new StringBuilder();
+
         try {
             Connection con = ConnectionFactory.connection();
             Statement st = con.createStatement();
-            ResultSet rst = st.executeQuery("SELECT * FROM CITY");
+            String idParam = req.getParameter("id");
 
-
-            while (rst.next()) {
-                int id = rst.getInt(1);
-                String name = rst.getString(2);
-                stringBuilder.append(id + ". " + name + "<br>");
-                list.add(id);
+            if (idParam != null) {
+                int idDelete = Integer.parseInt(idParam);
+                st.executeUpdate("DELETE FROM CITY where ID =" + idDelete);
+                stringBuilder.append(idDelete + " = element deleted " + "<br>");
+            } else {
+                ResultSet rst = st.executeQuery("SELECT * FROM CITY");
+                while (rst.next()) {
+                    int id = rst.getInt(1);
+                    String name = rst.getString(2);
+                    stringBuilder.append(id + ". " + name + "<br>");
+                }
             }
+
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -42,7 +48,6 @@ public class CityServlet extends HttpServlet {
                 "</head>\n" +
                 "<body>\n" +
                 "<div style='width: 50%; float: left;'>" + s + "<br>" + "</div>\n" +
-                "<div style='width: 50%; float: left;'> sum of cites =" + list.size() + "</div>\n" +
                 "</body>\n" +
                 "</html>");
     }
